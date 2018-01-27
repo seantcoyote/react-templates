da68721d76a4c5ca9c566e88047665dc014ac004
seantcoyote <seantcoyote@gmail.com>
2018-01-26 17:51:52 -0800

    Initial commit

---
+++ add-standard-stuff/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default { // Get from Firebase control panel
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "",
+  messagingSenderId: "messagingSenderId"
+}
